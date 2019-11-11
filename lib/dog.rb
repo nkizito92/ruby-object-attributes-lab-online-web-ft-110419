21da7a794a4fb7a2abcbbe8fb65747bfb4616024
Kizito Njoku <nkizito57@gmail.com>
2019-11-11 21:12:38 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,12 @@
 class Dog 
   def initialize(name, breed)
-    
+    @name = name
+    @breed = breed 
   end
+  
+  def info = (dogs_info)
+    name, breed = dogs_info
+    @name = name 
+    @breed = breed 
+  end 
 end

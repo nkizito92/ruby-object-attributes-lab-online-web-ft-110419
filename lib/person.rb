a3b83255c2e49b1a62c16bdd87f4dcefb4da73b8
Kizito Njoku <nkizito57@gmail.com>
2019-11-11 21:28:43 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,16 @@
+class Person 
+  def name=(name)
+
+    @name = name 
+  end 
+  def job=(breed)
+    @breed = breed 
+  end 
+  
+  def name 
+    @name
+  end 
+  def breed 
+    @breed
+  end 
+end
